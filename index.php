7ea7f2b245a052bb139b9224ba3ba826697119a4
Victor Nicollet <victor@nicollet.net>
2013-04-16 22:21:05 +0200

    [Index] Dispatcher

--- index.php
+++ index.php
@@ -5,3 +5,27 @@ define('URI', $_SERVER['REQUEST_URI']);
 
 // We support GET and POST methods
 define('METHOD', $_SERVER['REQUEST_METHOD']);
+
+if ( METHOD == 'POST' )
+{
+	if ( URI == '/upload' ) 
+	{
+		require_once 'upload.php';
+	}
+	else if ( URI == '/delete' )
+	{
+		require_once 'delete.php';
+	}
+	else if ( URI == '/meta' )
+	{
+		require_once 'meta.php';
+	}
+	else
+	{
+		require_once 'error.php';
+	}
+}
+else
+{
+	require_once 'download.php';
+}
