71f2ece233dec92d3c786c931bd3d8dba4b0fd7d
Victor Nicollet <victor@nicollet.net>
2013-04-16 23:14:44 +0200

    [Upload] Preparation controller

---
+++ prepare.php
@@ -0,0 +1,32 @@
+<?php
+
+$request = (object) array(
+	
+	// The path where the file can be uploaded, and from where it
+	// will be downloadable. For instance, "/my/path.png"
+	"path" => $_POST["path"],
+	
+	// The *maximum* size of the uploaded file, in bytes.
+	"size" => (int)$_POST["size"],
+	
+	// The time when this prepared upload expires, expressed as
+	// an ISO-8601 date time with no timezone.
+	"ends" => date('Y-m-d\TH:i:s\Z',strtotime($_POST["ends"]))
+	
+);
+
+$hmac = $_GET["HMAC"];
+$json = json_encode($request);
+
+$hmac_is_correct = ( hash_hmac("sha1",$json,API_KEY) == $hmac );
+
+if ( !$hmac_is_correct )
+{
+	error("403 Forbidden");
+}
+else
+{
+	require_once 'model.php';
+	$token = prepare_upload($request);
+	respond(array("status" => "ok", "token" => $token));
+}
