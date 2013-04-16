b86590dc327e31457053c221be5172fdc2a46e8a
Victor Nicollet <victor@nicollet.net>
2013-04-16 23:11:29 +0200

    [Model] Initial definition

---
+++ model.php
@@ -0,0 +1,84 @@
+<?php
+
+// The server-side path where the document is stored.
+// Uses an MD5 hash to avoid wasting time on path sanitization
+// and directory structure upkeep.
+function file_path($path)
+{
+	return "uploads/" . md5($path);
+}
+
+// The meta file sits next to the main file, and contains
+// general information about that file :
+//   - actual name (the name it was uploaded as)
+//   - MIME type
+function meta_path($path)
+{
+	return file_path($path).".meta";
+}
+
+// The upload file is created before the actual file, and
+// determines : 
+//   - how long until the upload permission expires ?
+//   - how large can the uploaded file be ?
+//   - what is the secret key token for uploading ? 
+function upload_path($path)
+{
+	return file_path($path).".upload";
+} 
+
+// Prepare uploading a file by saving a '*.upload' file
+// to the disk. Returns the policy token for uploading.
+function prepare_upload($policy)
+{
+	$upload_path = upload_path($policy->path);
+	$policy->token = sha1(uniqid(mt_rand(), true));
+	$json = json_encode($policy);
+	file_put_contents($upload_path,$json);
+	return $policy->token;
+}
+
+// Returns true if a given upload was permitted,
+// saves the data to the disk.
+function perform_upload($path,$token,$size,$file,$filename)
+{
+	$json = @file_get_contents(upload_path($path));
+	if ( !$json ) return false;
+	
+	$policy = @json_decode($json);
+	if ( !$policy ) return false;
+	
+	if ( $policy->token != $token ) return false;
+	
+	if ( $policy->size < $size ) return false;
+	
+	if ( $policy->ends < NOW ) return false;
+	
+	$meta = array("name" => $filename);
+	
+	@file_put_contents(file_path($path), $file);
+	@file_put_contents(meta_path($path), json_encode($meta));
+	@unlink(upload_path($path));
+	
+	return true;
+}
+
+// Set the mime-type of a file. Returns false if
+// it failed.
+function set_mime($path,$mime)
+{
+	$metapath = meta_path($path);
+	
+	$json = @file_get_contents($metapath);
+	if ( !$json ) return false;
+	
+	$meta = @json_decode($json);
+	if ( !$meta ) return false;
+	
+	$meta->mime = $mime;
+	
+	@file_put_contents($metapath, json_encode($meta));
+	
+	return true;
+}
+
